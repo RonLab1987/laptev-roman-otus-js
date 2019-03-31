150d5bc5e652a952100a3d8f96cbccdb71faa3d8
Roman Laptev <rlaptev@infoss.no>
2019-03-31 11:14:04 +0300

    Transform result to number (from instance of Number class)

--- javascript-1/index.js
+++ javascript-1/index.js
@@ -1,6 +1,6 @@
 export const accumulator = (function accumulator(addend) {
   if (addend === undefined) {
-    return this
+    return Number(this)
   } else {
     const result = this + addend
     return accumulator.bind(!isNaN(result) ? result : this)
